7ea16533f85b325514ed436622f09ddbd651c5b4
dkstarlife <dkstarlife@yahoo.dk>
2015-10-06 12:12:54 +0200

    yoloswag

--- PeriodicSystem/Model/Atom.cs
+++ PeriodicSystem/Model/Atom.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Model
 {
     public class Atom : IAtom{
-
+        Point pos;
     }
 }
