a906695b5e8a08649c7cfa10af4ff50ecfb84d0b
dkstarlife <dkstarlife@yahoo.dk>
2015-10-20 09:23:38 +0200

    slightly more yolo

---
+++ PeriodicSystem/PeriodicSystem/PElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Controls;
+
+namespace PeriodicSystem
+{
+    class PElement : UserControl{
+        Label[] labels;
+        String symbol { get { return symbol; } set { symbol = value; } }
+        int number { get { return number; } set { number = value; } }
+        double weight { get { return weight; } set { weight = value; } }
+        int[] shells { get { return shells; } set { shells = value; } }
+
+        public PElement(String symbol, int number, double weight, int[] shells){
+
+            this.symbol = symbol;
+            this.number = number;
+            this.weight = weight;
+            this.shells = shells;
+
+        }
+
+    }
+}
