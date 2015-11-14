a6cece037986b5270ba52e2adfb307e83d240166
TotalOfferen <jonas@groggy.dk>
2015-11-14 14:50:48 +0100

    Atomer kan ikke flyttes ud af deres box Tegning af atomer binder til modellens højde Menu

--- PeriodicSystem/Model/Atom.cs
+++ PeriodicSystem/Model/Atom.cs
@@ -9,11 +9,29 @@ namespace Model
 {
     public class Atom : NotifyBase
     {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
         private double x = 0;
         private double y = 0;
 
-        public double X { get { return x; } set { x = value; CenterX = value+50; NotifyPropertyChanged(); } }
-        public double Y { get { return y; } set { y = value; CenterY = value + 50; NotifyPropertyChanged(); } }
+        public double X {
+            get { return x; }
+            set {
+                x = (value < 0) ? 0 : value;
+                CenterX = x+(Width/2);
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double Y {
+            get { return y; }
+            set {
+                y = (value < 0) ? 0 : value;
+                CenterY = y + (Height/2);
+                NotifyPropertyChanged();
+            }
+        }
 
         private double centerX = 0;
         private double centerY = 0;
@@ -32,6 +50,8 @@ namespace Model
             X = 220;
             Y = 220;
             Protons = protons;
+            Width = 100;
+            Height = 100;
         }
 
         public Atom(int protons, int x, int y)
@@ -39,6 +59,8 @@ namespace Model
             X = x;
             Y = y;
             Protons = protons;
+            Width = 100;
+            Height = 100;
         }
 
         //public event PropertyChangedEventHandler PropertyChanged;
